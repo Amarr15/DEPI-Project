5864cd68f5ce621c25ea2b05eb6605bfc7f9b790
Abdalrahman Elegail <122580816+AbdalrahmanElegail@users.noreply.github.com>
2025-04-11 17:04:24 +0200

    Create Donation Db Using Entity Framework
    Create Donation Db Using Entity Framework as Code First

---
+++ DonationDb/EntityDbCodeFirst/DonationDB/DonationDB/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DonationDB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
